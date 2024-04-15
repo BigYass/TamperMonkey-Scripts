1328e279be4208d5d574ea8b1a4e05fa58078e2a
Yassine <yorkparodie@gmail.com>
2024-04-16 01:13:57 +0200

    first commit

---
+++ BottomBarRemover.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         BottomBarRemover
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-15
+// @description  Remove "you are responsable" bottom bar
+// @author       BigYass
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+  'use strict';
+
+  console.log("[BBR] Starting...");
+
+  let interval_id = null
+  let interval_left = 10
+
+  const removeBar = () => {
+    if (interval_left-- <= 0) {
+      clearInterval(interval_id)
+      return
+    }
+
+    const bottom_bars = document.querySelectorAll("ytd-permission-role-bottom-bar-renderer")
+
+    let removed = 0
+
+    bottom_bars.forEach((bar) => {
+      bar.remove()
+      removed++
+    });
+
+    if (removed === 0) {
+      console.log("[BBR] No bottom bar found... Trying again (" + interval_left + " left)")
+    } else {
+      console.log("[BBR] " + removed + " bars removed!")
+      clearInterval(interval_id)
+    }
+  };
+
+  interval_id = setInterval(removeBar, 1000)
+})();
